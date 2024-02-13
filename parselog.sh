bc7c3c3b3586b6cea72f6ced9eecc3d9524f4b95
root <root@pi-star>
2024-02-13 13:47:46 -0500

    updated

--- parselog.sh
+++ parselog.sh
@@ -14,27 +14,27 @@ fi
 ##### Main program #########
 #F1=$(tail -n 1000 /var/log/pi-star/MMDVM-20*  )
 F1=$(ls -alt /var/log/pi-star/MMDVM-20* | tail -n 1 | cut -d " " -f9)
-echo "$F1" 
+#echo "$F1" 
 DT=$(cat $F1| tail -n 1 | cut -d " " -f2)
-echo "$DT"
+#echo "$DT"
 Hr=$(cat $F1| tail -n 1 | cut -d " " -f3 | cut -d ":" -f1)
 Mi=$(cat $F1| tail -n 1 | cut -d " " -f3 | cut -d ":" -f2)
 Tm="$Hr:$Mi"
 
-echo "$Tm"
+Calls=$(grep "2024-02-13 [$T1-$T2]" "$F1" | grep 'transmission from' | cut -d " " -f 2,3,14 | sort -k3 | uniq -f 2 -u)
 
-#Calls=$(grep "2024-02-13 [10:00-14:59]" "$F1" | grep 'transmission from' | cut -d " " -f 2,3,14 | sort | uniq)
-Calls=$(grep "2024-02-13 [$T1-$T2]" "$F1" | grep 'transmission from' | cut -d " " -f 2,3,14 | sort -k3)
+IFS==$(echo -en "\n\b")
+
+for item in $Calls
+do
+        Line=$(echo "$item")
+	Call=$(echo "$item" | cut -d " " -f3)
+	Line2=$(grep "$Call" /usr/local/etc/stripped.csv | tail -1 | cut -d "," -f3,4,5,6,7 )
+	echo "$Line   $Line2"
+	
+done
+
+echo "Number of Calls $Calls" | wc -l 
 
 
-if [ "$S" == 1 ]; then
-	Calls2=$(echo "$Calls" | sort -k2 | uniq -u)
 
-else
-	Calls2=$(echo "$Calls" | uniq -f 2 -u)
-
-
-fi
-echo "$Calls2"
-
-echo "Count = $Calls2" |wc -l
